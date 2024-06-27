02d6dfd6b216805aceffe0c495f6d8e43a68a394
Dieudonne000 <munezadieudonne2021@gmail.com>
2024-06-27 19:32:51 +0200

    fix error

--- Frontend/app/src/App.jsx
+++ Frontend/app/src/App.jsx
@@ -1,14 +1,16 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Home';
 import Contacts from './contacts';
+import './App.css'
+import './index.css'
 
 const App = () => (
-    <Router>
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/contacts" element={<Contacts />} />
-        </Routes>
-    </Router>
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/contacts" element={<Contacts />} />
+    </Routes>
+  </Router>
 );
 
 export default App;
--- backend/contactmodel.js
+++ backend/contactmodel.js
@@ -1,23 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const contactSchema = new mongoose.Schema({
-    title: String,
-    firstName: String,
-    lastName: String,
-    position: String,
-    company: String,
-    businessArena: String,
-    employees: Number,
-    street: String,
-    additionalInfo: String,
-    zipCode: String,
-    place: String,
-    country: String,
-    phoneCode: String,
-    phoneNumber: String,
-    email: String
+  title: String,
+  firstName: String,
+  lastName: String,
+  position: String,
+  company: String,
+  businessArena: String,
+  employees: Number,
+  street: String,
+  additionalInfo: String,
+  zipCode: String,
+  place: String,
+  country: String,
+  phoneCode: String,
+  phoneNumber: String,
+  email: String,
 });
 
-const Contact = mongoose.model('Contact', contactSchema);
+const Contact = mongoose.model("Contact", contactSchema);
 
 module.exports = Contact;
